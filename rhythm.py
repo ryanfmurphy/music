e34887d84f2e4f6ccc3fbeaf44043b7d4f0bebdc
murftown <ryanfmurphy@gmail.com>
2015-08-14 12:07:28 -0500

    dev/py/midi/rhythm.py - rhythm: save to db

--- rhythm.py
+++ rhythm.py
@@ -9,10 +9,12 @@
 '''
 
 from __future__ import print_function
-from getkey import getch
+from getkey import getch, nb_getch, NonBlockingGetch
 import datetime, time
 from fractions import Fraction
 import midi
+try:                import my_db
+except ImportError: my_db = None
 
 
 class Beat(object):
@@ -90,24 +92,25 @@ class Beat(object):
         else:
             return Fraction(1)
 
-    def print_which_beat(self):
+    def print_which_beat(self, offset=30, line_width=20):
         beat = self.which_beat_guess + 1
         if beat == 1:
-            print('-'*20)
-        print_fraction_as_mixed_number(beat)
+            print(' '*offset + '-'*line_width)
+        offset2 = (offset + line_width/2)
+        print(' '*offset2 + mixed_number_str(beat))
 
 
-def fraction_as_mixed_number(frac):
+def mixed_number(frac):
     whole_num = int(frac)
     leftover_frac = frac - whole_num
     return whole_num, leftover_frac
 
-def print_fraction_as_mixed_number(frac):
-    whole_num, leftover_frac = fraction_as_mixed_number(frac)
-    if leftover_frac:
-        print(whole_num, leftover_frac)
+def mixed_number_str(frac):
+    m = mixed_number(frac)
+    if m[1]:
+        return str(m[0]) + ' ' + str(m[1])
     else:
-        print(whole_num)
+        return str(m[0])
 
 
 def beats_sum_time_len(beats):
@@ -138,57 +141,91 @@ def listen():
     adjusted_pitches.next() # prime the coroutine
     note_chars = []
 
-    while True:
-        prev_beat = beat
+    with NonBlockingGetch():
+        while True:
+            prev_beat = beat
 
-        # waits for user tap
-        ch = getch()
-        if ch == 'q':
-            print('Quitting...')
-            note_strn = ''.join(note_chars)
-            return (beats, note_strn)
+            # waits for user tap -
 
-        # timing
-        if len(beats) > 5: # only feed it the rolling avg once we're confident in it
-            beat = Beat(prev_beat, rolling_beat_len_guess)
-        else:
-            beat = Beat(prev_beat)
+            # non-blocking version:
+            while True:
+                time.sleep(.002)
+                ch = nb_getch()
+                if ch: break
+            # blocking version:
+            #ch = getch()
 
-        # record the space before the note if any
-        if prev_beat and beat.delta_len_guess:
-            subdivision_factor = 2
-            remaining_time_slots = int(beat.delta_len_guess * subdivision_factor) - 1
-            note_chars.append('-' * remaining_time_slots)
-        # record the note itself
-        note_chars.append(ch)
+            if ch == 'q':
+                print('Quitting...')
+                note_strn = ''.join(note_chars)
+                return (beats, note_strn)
 
-        # play music
-        midi.note_off(prev_note)
-        if ch in midi.note_numbers:
-            raw_pitch = midi.note_numbers[ch]
-            # get next adjusted pitch from coroutine
-            note = adjusted_pitches.send(raw_pitch)
-        else:
-            note = None
-            print("No note for key", ch)
-        midi.note_on(note)
-        prev_note = note
+            # timing
+            if len(beats) > 5: # only feed it the rolling avg once we're confident in it
+                beat = Beat(prev_beat, rolling_beat_len_guess)
+            else:
+                beat = Beat(prev_beat)
 
-        # add to beat log
-        beats.append(beat)
-        # rolling avg beat length guess
-        deltas = beats[-10:]
-        rolling_beat_len_guess = rolling_avg_beat_len(deltas)
+            # record the space before the note if any
+            if prev_beat and beat.delta_len_guess:
+                subdivision_factor = 2
+                remaining_time_slots = int(beat.delta_len_guess * subdivision_factor) - 1
+                note_chars.append('-' * remaining_time_slots)
+            # record the note itself
+            note_chars.append(ch)
 
-        # display
-        #print(beat.delta, '-', beat)
-        #print('delta len guess:', beat.delta_len_guess)
-        #print('rolling beat len guess:', rolling_beat_len_guess)
-        beat.print_which_beat()
-        
+            # play music
+            midi.note_off(prev_note)
+            if ch in midi.note_numbers:
+                raw_pitch = midi.note_numbers[ch]
+                # get next adjusted pitch from coroutine
+                note = adjusted_pitches.send(raw_pitch)
+            else:
+                note = None
+                print("No note for key", ch)
+            midi.note_on(note)
+            prev_note = note
 
-if __name__ == '__main__':
+            # add to beat log
+            beats.append(beat)
+            # rolling avg beat length guess
+            deltas = beats[-10:]
+            rolling_beat_len_guess = rolling_avg_beat_len(deltas)
+
+            # display
+            #print(beat.delta, '-', beat)
+            #print('delta len guess:', beat.delta_len_guess)
+            #print('rolling beat len guess:', rolling_beat_len_guess)
+            beat.print_which_beat()
+            print(midi.show_list_spatially((note,), None, offset=30))
+
+def listen_and_save():
     beats, note_strn = listen()
     print(note_strn)
+    if my_db:
+        user_input = raw_input('Save to database? ')
+        save = False
+        if user_input in ('cut','shave'):
+            indexes = raw_input("Indexes (e.g. [:-1]): ")
+            note_strn = eval('note_strn' + indexes)
+            save = True
+        if is_a_yes(user_input):
+            save = True
+        if save:
+            result = save_melody_to_db(note_strn)
+            print("Saved (at least I think so!)")
+        
+def is_a_yes(strn):
+    return (strn.lower() in ('y','yes'))
 
+def save_melody_to_db(note_strn):
+    conn,cur = my_db.cur_db()
+    #result = cur.execute('INSERT INTO melody (melody) VALUES ($melody$'+note_strn+'$melody$);')
+    #cur.execute('PREPARE insert_melody AS INSERT INTO melody (melody) VALUES (?);')
+    result = cur.execute('INSERT INTO melody (melody) VALUES (%s);', (note_strn,))
+    conn.commit()
+    return result
+
+if __name__ == '__main__':
+    listen_and_save()
 
