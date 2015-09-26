2603542f4436a00464297396bf3d62f81e8f4fbf
ryanfmurphy <ryanfmurphy@gmail.com>
2015-09-26 02:56:17 -0500

    MusicConsole(), chords on different channel, rand_inst change

--- fns.py
+++ fns.py
@@ -1,5 +1,12 @@
 from midi import *
-import types, random
+import types, random, code, ast, _ast
+import midi
+
+CHORD_VEL = 65
+
+cur_chord = None
+prev_chord = None
+
 
 def under_assumption(assumption):
     if assumption == 'working with strings':
@@ -30,14 +37,19 @@ def get_funcs(env):
 def play_funcs(env):
     if is_module(env):
         env = env.__dict__
-    for func_name,func in get_funcs(env).items():
+    funcs = get_funcs(env).items()
+    random.shuffle(funcs)
+    for func_name,func in funcs:
         play_func(func)
 
-def with_pause_after(mel):
-    return mel + '-'
+def pause_amt(at_least=1):
+    low, high = at_least, at_least+3
+    return random.randint(low, high)
 
-def call_and_response(mel1,mel2):
-    return mel1 + '-' + mel2 + '-'
+def with_pause_after(mel, pause=None):
+    if pause is None:
+        pause = pause_amt()
+    return mel + '-' * pause
 
 def fname2mus_strn(fname):
     return fname.replace('_','-')
@@ -48,29 +60,63 @@ def print_fname(fname):
 def print_response(response):
     print ' '*60 + response
 
-def play_func(fn):
 
+def get_fname(fn):
     if is_function(fn):
-        fname = fn.__name__
+        return fn.__name__
     else:
-        fname = fn
+        return fn
+
+
+def play_func(fn, do_response=None):
+    global cur_chord, prev_chord
+
+    fname = get_fname(fn)
 
     if not is_lambda(fname):
 
         print_fname(fname)
         fname = fname2mus_strn(fname)
+        pause1 = pause_amt()
         play_strn(
-            with_pause_after(fname)
+            with_pause_after(fname, pause1),
+            show_notes = False,
         )
 
-        do_response = coinflip()
-        if do_response:
+        if do_response is None:
+            do_response = True #coinflip()
+
+        if do_response and is_function(fn):
             response = fn()
-            response = fname2mus_strn(response)
-            print_response(response)
-            play_strn(
-                with_pause_after(response)
-            )
+            if isinstance(response, types.GeneratorType):
+                for section in response:
+                    if cur_chord != prev_chord:
+                        midi.chordname_off(prev_chord, chan=1)
+                        midi.chordname_on(cur_chord, vel=CHORD_VEL, chan=1)
+                        print "chord:", cur_chord
+                        prev_chord = cur_chord
+                    print_response(section)
+                    pause2 = pause_amt(at_least = pause1)
+                    play_strn(
+                        with_pause_after(section, pause2),
+                        show_notes = False,
+                    )
+            else:
+                #todo make this into a function:
+                if cur_chord != prev_chord:
+                    midi.chordname_off(prev_chord, chan=1)
+                    midi.chordname_on(cur_chord, vel=CHORD_VEL, chan=1)
+                    print "chord:", cur_chord
+                    prev_chord = cur_chord
+                print_response(response)
+                pause2 = pause_amt(at_least = pause1)
+                play_strn(
+                    with_pause_after(response, pause2),
+                    show_notes = False,
+                )
+
+def musicall(fn): #todo args
+    play_func(fn, do_response=True)
 
 def coinflip(unlikeliness=2):
     return random.randint(1,unlikeliness) == 1
@@ -98,4 +144,124 @@ def goof_around(env):
             play_funcs(env)
     except KeyboardInterrupt:
         print "Bye!"
+        midi.panic()
+
+def take_from_env(names, env):
+    return {name: env[name] for name in names}
+
+class MusicConsole(code.InteractiveConsole):
+
+    def runsource(self, source, filename='<input>', symbol='single'):
+        # code taken from InteractiveInterpreter.runsource in code.py
+        try:
+            code = self.compile(source, filename, symbol)
+        except (OverflowError, SyntaxError, ValueError):
+            # Case 1
+            self.showsyntaxerror(filename)
+            return False
+
+        if code is None:
+            # Case 2
+            return True
+
+        # Case 3
+        self.maybe_make_music(source)
+        self.runcode(code, source)
+        return False
+
+    def runcode(self, the_code, source, filename='<input>'):
+        # code taken from InteractiveInterpreter.runsource in code.py
+        try:
+            tree = ast.parse(source)
+            try:
+                expr = ast.parse(source, mode='eval')
+            except:
+                expr = None
+            #todo get this to work for multiple expr's, not just 1:
+            if expr and len(tree.body) == 1:
+                # _ = expr_value
+                tree.body[0] = ast_wrap_in_assn('_', tree.body[0])
+                # print _
+                underscore = _ast.Name(id="_", ctx=_ast.Load())
+                print_node = ast_print_node([_ast.Str(s=' '*50), underscore])
+                tree.body.append(print_node)
+                # play_whatever
+                play_whatever_node = ast_call_node('midi.play_whatever', '_', show_notes=False)
+                tree.body.append(play_whatever_node)
+            #print ast.dump(tree)
+            code_obj = compile(tree, '<input>', 'exec')
+            exec code_obj in self.locals
+        except SystemExit:
+            raise
+        except:
+            self.showtraceback()
+        else:
+            if code.softspace(sys.stdout, 0):
+                print
+
+    def maybe_make_music(self, source):
+        tree = ast.parse(source)
+        #print ast.dump(tree)
+        fn = ast_funcall_to_fname(tree)
+        if fn:
+            #print "yep! functioncall"
+            play_func(fn)
+
+def ast_wrap_in_assn(var_name, ast_expr):
+    assn = _ast.Assign(
+        targets = [_ast.Name(id=var_name, ctx=_ast.Store())],
+        value = ast_expr.value
+    )
+    ast.fix_missing_locations(assn)
+    return assn
+
+def ast_parse1(source):
+    return ast.parse(source).body[0]
+
+def ast_expr(tree):
+    if isinstance(tree, _ast.Module):
+        body = tree.body
+        if len(body) == 1:
+            item = body[0]
+            if isinstance(item, _ast.Expr):
+                return item
+
+def ast_funcall_to_fname(tree):
+    expr = ast_expr(tree)
+    if expr:
+        if isinstance(expr.value, _ast.Call):
+            call = expr.value
+            return call.func.id
+
+def ast_print_node(values):
+    print_node = _ast.Print(
+        values=values,
+        nl=True
+    )
+    ast.fix_missing_locations(print_node)
+    return print_node
+
+def ast_call_node(fname, *args, **kwargs):
+    innards = ','.join(str(arg) for arg in args)
+    kwinnards = ','.join(str(kw)+'='+str(val) for kw,val in kwargs.items())
+    if kwinnards:
+        innards += ',' + kwinnards
+    source = fname+'('+innards+')'
+    #print "source:", source
+    call = ast_parse1(source)
+    ast.fix_missing_locations(call)
+    return call
+
+#todo get console to have up-arrow history (readline)
+    # actually it works with python -i
+def console(env):
+    try:
+        MusicConsole(env).interact()
+    except KeyboardInterrupt:
+        midi.panic()
+        print "See ya!"
+
+def setup():
+    midi.rand_inst(chan=0)
+    midi.rand_inst(chan=1)
 
--- musictxt.py
+++ musictxt.py
@@ -1,10 +1,12 @@
-import midi, types, random, fns, pdb
+import midi, types, random, fns, pdb, musictxt
 
 def ceg():
+    fns.cur_chord = "C"
     if fns.coinflip():
         response = fns.options(*"cdefgaBC")
     else:
         response = "dBG"
+        fns.cur_chord = "G major"
     if fns.coinflip():
         return gCgfe(response)
     else:
@@ -13,15 +15,19 @@ def ceg():
 def edceg():
     return 'fed'
 
+
 def gCgfe(gef=None, fde=None):
+    fns.cur_chord = "C"
     if gef == "dBG":
         if fde == "cAG":
             return "ef|gCgfe"
         elif fde is 'fde':
             return "cAG";
         elif fde is None:
+            fns.cur_chord = "G7"
             return 'fde-cAG-';
-    elif fde == "gafed":
+
+    if fde == "gafed":
         return "G-G-G---"
     elif (gef,fde) == ('d','e'):
         return 'c','A'
@@ -32,19 +38,76 @@ def gCgfe(gef=None, fde=None):
                         "abC","baf","ede","eeA")
 
 def edcHA():
+    fns.cur_chord = fns.options('Dm','F','Bb')
     if fns.under_assumption('working with strings'):
         return 'fe-d'
     else:
         return ('f','e',None,'d')
 
 def cefg():
+    if fns.coinflip():
+        fns.cur_chord = fns.options('C','Em','Am','Cmaj7','Em7','Am7')
     parts = ('fed-','cBA-')
     return fns.some_parts(*parts)
 
 def cBA_():
+    fns.cur_chord = fns.options('Am','Am7')
     parts = ('fed-', 'cdefe')
     return fns.some_parts(*parts)
 
+def aCF(): return '-CaCf' # leading hyphen means no pause
+def bDG(): return '-DbDg'
+def CEA():
+    fns.cur_chord = fns.options('D7', 'Am')
+    return '-OABcAMED-D---'
+
+def edc():
+    if fns.coinflip():
+        fns.cur_chord = fns.options('Dm', 'G')
+        return '-defg---'
+    else:
+        fns.cur_chord = fns.options('C', 'Em')
+        return 'efg'
+
+def abC():
+    if fns.coinflip():
+        return '-b-a-g---'
+    else:
+        return '-b-g-e---'
+
+def fga():
+    r = random.randint(1,4)
+    if r==1:
+        fns.cur_chord = fns.options('D7', 'F#m7b5', 'B7')
+        return 'mga'
+    elif r==2:
+        fns.cur_chord = fns.options('C','F')
+        return 'gec'
+    elif r==3:
+        fns.cur_chord = 'Am'
+        return 'abC'
+    elif r==4:
+        return 'abCbabCDE-'
+    else:
+        raise ValueError("Unexpected random r value " + r)
+
+def cde():
+    yield "def"
+    yield "efg"
+    yield "fga"
+    fns.cur_chord = 'F'
+    yield "gab"
+    yield "abC"
+    fns.cur_chord = 'G'
+    yield "bCD"
+    fns.cur_chord = 'C'
+    yield "C"
+
 if __name__ == '__main__':
-    fns.goof_around(globals())
+    env = globals()
+    #names = ('ceg',)
+    #env = fns.take_from_env(names, globals())
+    fns.setup()
+    fns.goof_around(env)
+    fns.console(env = env)
 
