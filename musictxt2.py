8f26c2358b73e12253db3c4fd0550bc1691e7ba3
ryanfmurphy <ryanfmurphy@gmail.com>
2015-10-28 21:25:44 -0500

    cute little melody, not using SOMETIMES_DELAY, using swung rhythm and fn.start_chord

--- musictxt2.py
+++ musictxt2.py
@@ -1,5 +1,9 @@
 import midi, random, fns, pdb, musictxt
 
+fns.DURATION = midi.swung_dur(.3,.15).next
+fns.SOMETIMES_DELAY = False
+
+'''
 def cde():
     yield 'def'
     yield 'efg'
@@ -21,10 +25,26 @@ def cde():
         fns.cur_chord = 'C'
         yield 'c-----'
 cde.start_chord = 'C'
+'''
+
+def g__edc__():
+    yield 'efmgec--'
+g__edc__.start_chord = 'C'
+
+def fedcAcee():
+    fns.cur_chord = 'D7'
+    yield '-d------'
+fedcAcee.start_chord = 'Fmaj7'
 
 if __name__ == '__main__':
     env = globals()
     fns.setup()
     fns.goof_around(env)
     fns.console(env = env)
+
+#todo shorted cur_chord and start_chord to just chord
+#todo allow fn.start_chord to be a function
+#todo decorator for start_chord
+#todo only choose a new instrument if "new" is chosen as argv,
+    # else keep the existing ones
 
