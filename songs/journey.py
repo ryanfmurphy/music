a6e29e8456d4b8129718fdbdb926882fd29a4e61
ryanfmurphy <ryanfmurphy@gmail.com>
2015-11-27 20:00:29 -0600

    journey with chord subs / melody w jacqueline

--- songs/journey.py
+++ songs/journey.py
@@ -1,8 +1,10 @@
 import music.fns as f
 
 f.MEL_VEL = 100
-f.CHORD_VEL = 60
+f.CHORD_VEL = 70
 f.SOMETIMES_DELAY = False
+
+f.DURATION = .3
 
 @f.start_chord('C')
 def cdefgfga():
@@ -19,7 +21,26 @@ def cdefgfga():
     f.chord_to('F')
     yield 'afcA'
     f.chord_to('C')
-    yield 'cdeC-eC-edcAcdee'
+    yield 'cdeC-eC-'
+    f.chord_to('Am')
+    yield 'edcAcdee'
+
+@f.start_chord('C')
+def cH_cd_Hd():
+    f.chord_to(f.options('Bb','Gm'))
+    yield 'ef--'
+    f.chord_to('F')
+    yield '-fgh'
+    f.chord_to('C')
+    yield 'C--gChgf-fefgfeH'
+    f.chord_to('C')
+    yield 'c----cdf'
+    f.chord_to(f.options('Bb','Eb'))
+    yield 'hdch'
+    f.chord_to('F')
+    yield 'acHa'
+    f.chord_to(f.options('C','Am'))
+    yield 'g-edegedgCge-dcG'
 
 if __name__ == '__main__':
     env = globals()
