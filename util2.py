c7fa475a2be4022237fa64dae1a61ddeb6e8cb3e
ryanfmurphy <ryanfmurphy@gmail.com>
2016-03-24 07:38:27 -0500

    fn factoring in util2
    car, cdr, env_copy

--- util2.py
+++ util2.py
@@ -4,12 +4,23 @@ def is_listy(x):
     return isinstance(x,list) \
         or isinstance(x,tuple) #choose #fixme be more general
 
+def car(L):
+    return L[0]
+
+def cdr(L):
+    return L[1:]
+
+def env_copy():
+    return copy.copy(globals()) \
+               .update(locals())
+
 def lispy_funcall(l, env=None):
     # event e is a lispy tuple (fn arg1 arg2 ...)
     if env is None:
-        env = copy.copy(globals()).update(locals())
-    func_name = l[0]
-    args = l[1:]
+        env = env_copy()
+        # Q. will this make globals/locals that are edited not actually get edited?
+    func_name = car(l)
+    args = cdr(l)
     fn = env[func_name]
     return fn(*args)
 
