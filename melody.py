092a48217b28cf0bdc4aa9fc68b3e964558f4bbb
ryanfmurphy <ryanfmurphy@gmail.com>
2016-03-24 07:39:28 -0500

    executables rand_inst, groovy_chords
    small code cleanup on melody.py

--- melody.py
+++ melody.py
@@ -4,9 +4,9 @@ import my_db
 def get_db_melody(id=None):
     if id is None: # random - #todo #fixme what if it hits a hole?
         id = random.randint(1,my_db.count_rows('melody'))
-    melodyO = my_db.fetch_join_query(
-     'SELECT * FROM melody WHERE id='+str(id)
-    ).next()[0]
+    melodyO = my_db.fetch_join_query('''
+        select * from melody where id='''+str(id)+'''
+    ''').next()[0]
     return melodyO
 
 def play_db_melody(id=None):
